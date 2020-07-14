4f1d4fddb812afa3a0f4733552755b0c41ac2c85
Cristian Martínez <cristian.martinez@knarsas.com>
2020-07-14 18:36:45 -0500

    lógica de navegación

--- src/app/Components/filter/filter.component.ts
+++ src/app/Components/filter/filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-filter',
@@ -6,13 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.css']
 })
 export class FilterComponent implements OnInit {
+  @Output() sectionFlags = new EventEmitter<Object>();
+  private createVisible:boolean=true;
+  private textSectionFlag:boolean=false;
+  private dateSectionFlag:boolean=false;
+  private rangeSectionFlag:boolean=false;
+  
+  private textDisabled:boolean=false;
+  private dateDisabled:boolean=false;
+  private rangeDisabled:boolean=false;
 
   constructor() { }
 
   ngOnInit() {
   }
-  modelChanged(event){
-    console.log(event)
+  createForm(){
+    this.textDisabled = true; 
+    this.dateDisabled = true; 
+    this.rangeDisabled = true; 
+    let sections={
+      textSectionFlag:this.textSectionFlag,
+      dateSectionFlag:this.dateSectionFlag,
+      rangeSectionFlag:this.rangeSectionFlag
+    }
+    this.createVisible=false;
+    this.sectionFlags.emit(sections)
+
   }
 
 }
--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  private textSectionFlag:boolean=false;
+  private dateSectionFlag:boolean=false;
+  private rangeSectionFlag:boolean=false;
+  private sendVisible:boolean=false;
+  private createVisible:boolean=true;
+  private dataSectionFlag:boolean=false;
   constructor() { }
 
   ngOnInit() {
+  }
+  getSectionsFlags(sectionsFlags){
+    this.textSectionFlag=sectionsFlags.textSectionFlag;
+    this.dateSectionFlag=sectionsFlags.dateSectionFlag;
+    this.rangeSectionFlag=sectionsFlags.rangeSectionFlag;
+
+    this.sendVisible=true;
+  }
+  sendForm(){
+    this.textSectionFlag=false;
+    this.dateSectionFlag=false;
+    this.rangeSectionFlag=false;
+    this.dataSectionFlag=true;
+    this.sendVisible=false;
   }
 
 }
