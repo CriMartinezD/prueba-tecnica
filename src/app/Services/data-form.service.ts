24b6ef72eb423eeab76cc7e126b2ba87b22f2bf1
Cristian Martínez <cristian.martinez@knarsas.com>
2020-07-15 15:48:13 -0500

    Algunos comentarios

--- src/app/Services/data-form.service.ts
+++ src/app/Services/data-form.service.ts
@@ -9,24 +9,27 @@ export class DataFormService {
   date: Date;
 
   constructor() { }
+  //Función para obtener el json de visualización en HTML de forma "bonita"
   getDataHTML() {
-    console.log(this.date)
-    console.log(this.date.getUTCMonth())
     let dateToShow;
+    //Control de campo vacio y conversión del campo date a la visualización deseada
     if (this.date == null || this.date == undefined) {
       dateToShow = "00/00/0000";
     } else {
-      dateToShow = this.date.getUTCDate() + '/' + (this.date.getUTCMonth()+1) + '/' + this.date.getUTCFullYear();
+        dateToShow = this.date.getUTCDate() + '/' + (this.date.getUTCMonth()+1) + '/' + this.date.getUTCFullYear();
     }
+    //Generación de json de salida
     let dataform = {
       texto: this.text,
       fecha: dateToShow,
       cantidad: this.range.toString()
     }
+    //Conversión del json a una forma bonita en HTML
     return JSON.stringify(dataform, null, 4)
       .replace(/ /g, '&nbsp;')
       .replace(/\n/g, '<br/>');
   }
+  //Función para reiniciar las variables
   resetData() {
     this.text = ""
     this.range = 0;
